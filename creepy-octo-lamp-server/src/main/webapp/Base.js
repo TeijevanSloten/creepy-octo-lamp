1fe73aff98fa209eb15f4c32d9afbb178f5995a7
dolstrm001 <michel.dolstra@gmail.com>
2015-08-10 13:55:42 +0200

    make the gui socket reconnect on close

--- creepy-octo-lamp-server/src/main/webapp/Base.js
+++ creepy-octo-lamp-server/src/main/webapp/Base.js
@@ -2,14 +2,23 @@ angular.module('base', [])
         .controller('ClientController', function ($scope, $http) {
             this.selecterProps = null;
     
-            this.websock = new WebSocket("ws://localhost:8080/creepy-octo-lamp-server/wsGUI");
+            $scope.connect = function () {
+                return new WebSocket("ws://localhost:8080/creepy-octo-lamp-server/wsGUI");
+            };    
     
+            this.websock = $scope.connect();
+    
+            this.websock.onclose = function(event){
+                $scope.connect();
+            };
+            
             this.websock.onmessage = function(event){
                 console.log(event.data);
                 if(event.data === "updateClients"){
                     $scope.getClients();
                 }
             };
+            
             $scope.getClients = function () {
                 $http.get("http://localhost:8080/creepy-octo-lamp-server/resources/action/sessions")
                         .success(function (response) {
