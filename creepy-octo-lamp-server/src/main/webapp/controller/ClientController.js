8af53027465132b0fe0a727f90bedf30961fb889
Teije van Sloten <teije.van.sloten@hva.nl>
2015-08-14 22:29:45 +0200

    Removed websocket

--- creepy-octo-lamp-server/src/main/webapp/controller/ClientController.js
+++ creepy-octo-lamp-server/src/main/webapp/controller/ClientController.js
@@ -1,44 +1,34 @@
 app.registerCtrl('ClientController', function ($scope, $http) {
+
+    this.initialize = function(){
+        this.selecterProps = null;
+        this.getClients();
+    };
+
+
+    this.getClients = function () {
+        $http.get("resources/action/devices")
+            .success(function (response) {
+                $scope.clients = response;
+                console.log($scope.clients);
+            });
+    };
+
+
+    this.showProperties = function (index) {
+        if (this.selecterProps === null)
+            return false;
+        return this.selecterProps === index;
+    };
+
+    this.openProperties = function (index) {
+        console.log(index);
+        if (this.showProperties(index) === true) {
             this.selecterProps = null;
-                
-            $scope.connect = function () {
-                return new WebSocket("ws://localhost:8080/creepy-octo-lamp-server/wsGUI");
-            };    
-    
-            this.websock = $scope.connect();
-    
-            this.websock.onclose = function(event){
-                console.log(event);
-                $scope.connect();
-            };
-            
-            this.websock.onmessage = function(event){
-                console.log(event.data);
-                if(event.data === "updateClients"){
-                    $scope.getClients();
-                }
-            };
-            
-            $scope.getClients = function () {
-                $http.get("resources/action/devices")
-                        .success(function (response) {
-                            $scope.clients = response;
-                            console.log($scope.clients);
-                        });
-            };
-            
-            this.showProperties = function (index) {
-                if (this.selecterProps === null)
-                    return false;
-                return this.selecterProps === index;
-            };
-            
-            this.openProperties = function (index) {
-                console.log(index);
-                if (this.showProperties(index) === true) {
-                    this.selecterProps = null;
-                } else {
-                    this.selecterProps = index;
-                }
-            };
-        });
+        } else {
+            this.selecterProps = index;
+        }
+    };
+
+    this.initialize();
+});
