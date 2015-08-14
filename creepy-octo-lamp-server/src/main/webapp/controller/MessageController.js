18b2f267c227f0a6843ac5c2fb5087ec05d3d6fc
Teije van Sloten <teije.van.sloten@hva.nl>
2015-08-14 22:14:35 +0200

    Get replaced to post

--- creepy-octo-lamp-server/src/main/webapp/controller/MessageController.js
+++ creepy-octo-lamp-server/src/main/webapp/controller/MessageController.js
@@ -1,7 +1,3 @@
 app.registerCtrl('MessageController', function ($scope, $http) {
-    this.msg;
-    $http.get("http://localhost:8080/creepy-octo-lamp-server/resources/action/").
-            success(function (response) {
-                $scope.msg = response;
-            });
+    $http.post("http://localhost:8080/creepy-octo-lamp-server/resources/action/", {});
 });
